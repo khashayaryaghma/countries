a33b2604e0aca747266726d690999c8a77ea6a88
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-06-15 23:35:27 +0430

    Update getData.js

--- src/api/getData.js
+++ src/api/getData.js
@@ -4,8 +4,8 @@ const baseUrl = "https://restcountries.com/v2/all";
 
 export default async function getData(){
     try {
-        const dat= await fetch(baseUrl);
-        const data = await dat.json()
+        const res= await fetch(baseUrl);
+        const data = await res.json()
         return data;
     } catch (error) {
         console.log(error);
