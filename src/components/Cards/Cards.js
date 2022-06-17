ca642e4c8a7abef55de991893c3e7dfa7714cee9
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-06-17 22:16:54 +0430

    cards

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,6 +1,6 @@
 
 import { Card } from "react-bootstrap";
-
+import StyledCards from "./Cards.styles";
 
 
 export default function Cards({data}) {
@@ -9,21 +9,23 @@ export default function Cards({data}) {
       <>
           {data?.map((country) => {
               return (
-                  <Card style={{ width: "18rem" }}>
-                      <Card.Img
-                          variant="top"
-                          src={country.flags.svg}
-                          height="180"
-                      />
-                      <Card.Body>
-                          <Card.Title>{country.name}</Card.Title>
-                          <Card.Text>
-                              Population: {country.population}
-                          </Card.Text>
-                          <Card.Text>Region: {country.region}</Card.Text>
-                          <Card.Text>Capital: {country.capital}</Card.Text>
-                      </Card.Body>
-                  </Card>
+                  <StyledCards variant="boxShadow">
+                      <Card style={{ width: "18rem" }}>
+                          <Card.Img
+                              variant="top"
+                              src={country.flags.svg}
+                              height="180"
+                          />
+                          <Card.Body>
+                              <Card.Title>{country.name}</Card.Title>
+                              <Card.Text>
+                                  Population: {country.population}
+                              </Card.Text>
+                              <Card.Text>Region: {country.region}</Card.Text>
+                              <Card.Text>Capital: {country.capital}</Card.Text>
+                          </Card.Body>
+                      </Card>
+                  </StyledCards>
               );
           })}
       </>
