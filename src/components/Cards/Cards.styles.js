5003c7eaf7cc70bf1f6e2b9d759a256084484432
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-06-17 22:58:58 +0430

    transition

--- src/components/Cards/Cards.styles.js
+++ src/components/Cards/Cards.styles.js
@@ -2,5 +2,9 @@ import styled from "styled-components";
 
 const StyledCards = styled.div`
     box-shadow: ${(props)=>props.variant === "boxShadow" ? "rgb(0 0 0 / 15%) 0px 0px 10px 0px" : ""};
+    transition: all linear 0.1s;
+    &:hover{
+        transform: scale(1.04);
+    }
 `
 export default StyledCards;
