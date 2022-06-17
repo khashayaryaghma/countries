b0325ba168f57c7da77bfe00f403e9b3da3f9a2a
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-06-17 21:57:23 +0430

    Update Header.styles.js

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -2,6 +2,5 @@ import styled from "styled-components";
 
 const StyledHeader = styled.header`
     box-shadow: ${(props)=>props.variant === "boxShadow" ? "rgb(0 0 0 / 15%) 0px 0px 10px 0px" : ""};
-    z-index: 1;
 `;
 export default StyledHeader;
