c981ebebbf56115fabd58a7a18adc8d73745598d
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-06-17 19:48:53 +0430

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,34 @@
 import { useState } from "react";
-
+import styled from "styled-components";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+const styledHeader = styled.header`
+    
+`
 function Header() {
+    const [mode, setMode] = useState("Light Mode");
 
-    const[mode, setMode]=useState("Light Mode");
-
-    function changeMode(){
-
-        setMode(mode === "Light Mode" ? "Dark mode" : "Light Mode");
-
+    function changeMode() {
+        setMode(mode === "Light Mode" ? "Dark Mode" : "Light Mode");
     }
 
-  return (
-      <div className="d-flex p-4 justify-content-between">
-          <h1>Where in the world?</h1>
-          <div
-              onClick={changeMode}
-              role="button"
-              style={{ userSelect: "none" }}
-          >
-              {/* <FontAwesomeIcon icon="fa-solid fa-sun" /> */}
-              <span>{mode}</span>
-          </div>
-      </div>
-  );
+    return (
+        <styledHeader className="d-flex p-4 justify-content-between align-items-center">
+            <h1 style={{fontSize:"23px"}}>Where in the world?</h1>
+            <div
+                onClick={changeMode}
+                role="button"
+                style={{ userSelect: "none" }}
+            >
+                {/* {mode === "Light Mode" ? (
+                    <FontAwesomeIcon icon="fa-solid fa-sun" />
+                ) : (
+                    <FontAwesomeIcon icon="fa-solid fa-moon" />
+                )} */}
+
+                <span>{mode}</span>
+            </div>
+        </styledHeader>
+    );
 }
 
-export default Header
+export default Header;
