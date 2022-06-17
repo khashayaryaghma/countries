de6dd206d9dffbef323e4bd2be4721b6cd735a50
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-06-17 18:06:20 +0430

    light and dark

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,25 @@
-
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Header() {
+
+    const[mode, setMode]=useState("Light Mode");
+
+    function changeMode(){
+
+        setMode(mode === "Light Mode" ? "Dark mode" : "Light Mode");
+
+    }
+    
   return (
-    <div>Header</div>
-  )
+      <div>
+          <h1>Where in the world?</h1>
+          <div onClick={changeMode}>
+              <FontAwesomeIcon icon="fa-solid fa-sun" />
+              <span>{mode}</span>
+          </div>
+      </div>
+  );
 }
 
 export default Header
