ac08e1f890a9ff9f7efc411c5788725d2cfc1a4a
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-06-17 18:33:55 +0430

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Header() {
 
@@ -10,12 +11,16 @@ function Header() {
         setMode(mode === "Light Mode" ? "Dark mode" : "Light Mode");
 
     }
-    
+
   return (
-      <div>
+      <div className="d-flex p-4 justify-content-between">
           <h1>Where in the world?</h1>
-          <div onClick={changeMode}>
-              <FontAwesomeIcon icon="fa-solid fa-sun" />
+          <div
+              onClick={changeMode}
+              role="button"
+              style={{ userSelect: "none" }}
+          >
+              {/* <FontAwesomeIcon icon="fa-solid fa-sun" /> */}
               <span>{mode}</span>
           </div>
       </div>
