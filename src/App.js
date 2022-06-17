876da5d8094670fa95056cde55ec3285536b712e
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-06-17 17:28:12 +0430

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Cards from "./components/cards";
-
+import Header from "./components/Header";
 function App() {
 
     return (
       <div className="App">
+        <Header/>
         <Cards/>
       </div>
     );
