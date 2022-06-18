1efbc7e7fa7c554ccecd25087f0c1dd838219d2a
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-06-18 14:43:53 +0430

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,19 +1,18 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 // import getData from "../../api/getData";
-import InputGroup from "react-bootstrap/esm/InputGroup";
-import FormControl  from "react-bootstrap/esm/FormControl";
 import StyledMain from "./Main.styles";
 import Cards from "../Cards/Cards";
-import data from "../data";
+import Data from "../data";
 
 function Main() {
-    // const [data, setData]=useState([])
+    const [data, setData]=useState([])
 
-    // useEffect(() => {
-    //     getData().then((data) => {
-    //         setData(data)
-    //     });
-    // }, [data]);
+    useEffect(() => {
+        // getData().then((data) => {
+        //     setData(data)
+        // });
+        setData(Data);
+    }, [data]);
 
     return (
         <StyledMain bgColor="gray">
