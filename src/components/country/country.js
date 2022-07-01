d301b7b4aeac69f83e0ac3b4c068a0f0fa3f07c9
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-07-01 13:20:55 +0430

    useLocation

--- src/components/country/country.js
+++ src/components/country/country.js
@@ -1,9 +1,11 @@
-
+import { useLocation } from "react-router-dom"
 
 export default function Country() {
+  const {state} = useLocation();
+  console.log(state);
   return (
     <>
-        
+        {/* <h1>hi</h1> */}
     </>
   )
 }
