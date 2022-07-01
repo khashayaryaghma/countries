42cd182b5c0477ae3306d37b0a8a6b0d98a01f3a
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-07-01 13:26:15 +0430

    styled country

---
+++ src/components/country/Country.styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const StyledCountry = styled.section`
+
+`
+
+export default StyledCountry;
--- src/components/country/country.js
+++ src/components/country/country.js
@@ -1,11 +1,12 @@
 import { useLocation } from "react-router-dom"
+import StyledCountry from "./Country.styles";
 
 export default function Country() {
   const {state} = useLocation();
   console.log(state);
   return (
-    <>
-        {/* <h1>hi</h1> */}
-    </>
+      <StyledCountry>
+
+      </StyledCountry>
   )
 }
