4069fafbb1ce0a1b197512f06d910379567dca49
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-07-01 13:45:33 +0430

    Update country.js

--- src/components/country/country.js
+++ src/components/country/country.js
@@ -6,9 +6,26 @@ export default function Country() {
   console.log(state);
   return (
       <StyledCountry>
-        <div>
-          <Link to={"/"} className="" style={{textDecoration:"none", color:"inherit"}}>Back</Link>
-        </div>
+          <div>
+              <Link
+                  to={"/"}
+                  className="btn m-5"
+                  style={{
+                      textDecoration: "none",
+                      color: "inherit",
+                      boxShadow: "rgb(0 0 0 / 15%) 0px 0px 10px 0px",
+                      paddingLeft: "1.5rem",
+                      paddingRight: "1.5rem",
+                  }}
+              >
+                  Back
+              </Link>
+          </div>
+          <div>
+            <img src={state.flags.svg} alt="flag" />
+
+          </div>
+          
       </StyledCountry>
-  )
+  );
 }
