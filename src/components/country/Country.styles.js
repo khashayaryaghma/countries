490596142986ae37a3ee73558eee9ff4ed1cc7b1
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-07-04 01:12:08 +0430

    responsive

--- src/components/country/Country.styles.js
+++ src/components/country/Country.styles.js
@@ -6,4 +6,6 @@ export const StyledCountry = styled.section`
 
 export const StyledContainer = styled.section`
     display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
 `
--- src/components/country/country.js
+++ src/components/country/country.js
@@ -28,65 +28,77 @@ export default function Country() {
                     <img
                         src={state.flags.svg}
                         alt="flag"
-                        style={{ width: "28rem" }}
+                        style={{ width: "40vmax", marginBottom:"2rem", textAlign:"center" }}
                     />
                 </div>
-                <div>
-                    <h4>{state.name}</h4>
-                    <ul
-                        style={{
-                            listStyle: "none",
-                            lineHeight: "2.2rem",
-                            fontSize: ".9rem",
-                        }}
-                    >
-                        <li>Native Name: {state.nativeName}</li>
-                        <li>Population: {state.population}</li>
-                        <li>Region: {state.region}</li>
-                        <li>Sub Region: {state.subregion}</li>
-                        <li>Capital: {state.capital}</li>
-                    </ul>
-                </div>
-                <div>
-                    <ul
-                        style={{
-                            listStyle: "none",
-                            lineHeight: "2.2rem",
-                            fontSize: ".9rem",
-                        }}
-                    >
-                        <li>Top Level Domain: {state.topLevelDomain}</li>
-                        <li>
-                            Currencies:{" "}
-                            {state.currencies.map((ele) => ele.name + " ")}
-                        </li>
-                        <li>
-                            Languages:{" "}
-                            {state.languages.map((ele) => ele.name + " ")}
-                        </li>
-                    </ul>
-                </div>
-                {state.borders && (
-                    <div
-                        style={{ fontSize: ".9rem" }}
-                        className="d-flex align-items-baseline flex-wrap align-content-start"
-                    >
-                        Border Countries:
-                        {state.borders.map((ele) => (
-                            <span
-                                className="btn m-1"
+
+                <div style={{ marginLeft: "3rem" }}>
+                    <div className="d-flex flex-wrap align-items-center">
+                        <div>
+                            <h4>{state.name}</h4>
+                            <ul
                                 style={{
-                                    boxShadow:
-                                        "rgb(0 0 0 / 15%) 0px 0px 10px 0px",
-                                    fontSize: ".7rem",
+                                    listStyle: "none",
+                                    lineHeight: "2.2rem",
+                                    fontSize: ".9rem",
                                 }}
-                                key={uuidv4()}
                             >
-                                {ele}
-                            </span>
-                        ))}
+                                <li>Native Name: {state.nativeName}</li>
+                                <li>Population: {state.population}</li>
+                                <li>Region: {state.region}</li>
+                                <li>Sub Region: {state.subregion}</li>
+                                <li>Capital: {state.capital}</li>
+                            </ul>
+                        </div>
+
+                        <div>
+                            <ul
+                                style={{
+                                    listStyle: "none",
+                                    lineHeight: "2.2rem",
+                                    fontSize: ".9rem",
+                                }}
+                            >
+                                <li>
+                                    Top Level Domain: {state.topLevelDomain}
+                                </li>
+                                <li>
+                                    Currencies:{" "}
+                                    {state.currencies.map(
+                                        (ele) => ele.name + " "
+                                    )}
+                                </li>
+                                <li>
+                                    Languages:{" "}
+                                    {state.languages.map(
+                                        (ele) => ele.name + " "
+                                    )}
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                )}
+                    {state.borders && (
+                        <div
+                            style={{ fontSize: ".9rem" }}
+                            className="d-flex align-items-baseline flex-wrap align-content-start"
+                        >
+                            Border Countries:
+                            {state.borders.map((ele) => (
+                                <span
+                                    className="btn m-1 lowdark"
+                                    style={{
+                                        boxShadow:
+                                            "rgb(0 0 0 / 15%) 0px 0px 10px 0px",
+                                        fontSize: ".7rem",
+                                    }}
+                                    key={uuidv4()}
+                                >
+                                    {ele}
+                                </span>
+                            ))}
+                        </div>
+                    )}
+                </div>
             </StyledContainer>
         </StyledCountry>
     );
