8d5bb82d38e9f7f8771345ba93fd9e84b6c256b0
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-07-04 15:28:00 +0430

    Update country.js

--- src/components/country/country.js
+++ src/components/country/country.js
@@ -24,14 +24,18 @@ export default function Country() {
             </div>
 
             <StyledContainer className="highdark">
+                {state.flags.svg &&
                 <div>
                     <img
                         src={state.flags.svg}
                         alt="flag"
-                        style={{ width: "40vmax", marginBottom:"2rem", textAlign:"center" }}
+                        style={{
+                            width: "40vmax",
+                            marginBottom: "2rem",
+                            textAlign: "center",
+                        }}
                     />
-                </div>
-
+                </div>}
                 <div style={{ marginLeft: "3rem" }}>
                     <div className="d-flex flex-wrap align-items-center">
                         <div>
@@ -43,11 +47,21 @@ export default function Country() {
                                     fontSize: ".9rem",
                                 }}
                             >
-                                <li>Native Name: {state.nativeName}</li>
-                                <li>Population: {state.population}</li>
-                                <li>Region: {state.region}</li>
-                                <li>Sub Region: {state.subregion}</li>
-                                <li>Capital: {state.capital}</li>
+                                {state.nativeName && (
+                                    <li>Native Name: {state.nativeName}</li>
+                                )}
+                                {state.population && (
+                                    <li>Population: {state.population}</li>
+                                )}
+                                {state.region && (
+                                    <li>Region: {state.region}</li>
+                                )}
+                                {state.subregion && (
+                                    <li>Sub Region: {state.subregion}</li>
+                                )}
+                                {state.capital && (
+                                    <li>Capital: {state.capital}</li>
+                                )}
                             </ul>
                         </div>
 
@@ -59,21 +73,27 @@ export default function Country() {
                                     fontSize: ".9rem",
                                 }}
                             >
-                                <li>
-                                    Top Level Domain: {state.topLevelDomain}
-                                </li>
-                                <li>
-                                    Currencies:{" "}
-                                    {state.currencies.map(
-                                        (ele) => ele.name + " "
-                                    )}
-                                </li>
-                                <li>
-                                    Languages:{" "}
-                                    {state.languages.map(
-                                        (ele) => ele.name + " "
-                                    )}
-                                </li>
+                                {state.topLevelDomain && (
+                                    <li>
+                                        Top Level Domain: {state.topLevelDomain}
+                                    </li>
+                                )}
+                                {state.currencies && (
+                                    <li>
+                                        Currencies:{" "}
+                                        {state.currencies?.map(
+                                            (ele) => ele.name + " "
+                                        )}
+                                    </li>
+                                )}
+                                {state.languages && (
+                                    <li>
+                                        Languages:{" "}
+                                        {state.languages?.map(
+                                            (ele) => ele.name + " "
+                                        )}
+                                    </li>
+                                )}
                             </ul>
                         </div>
                     </div>
@@ -83,7 +103,7 @@ export default function Country() {
                             className="d-flex align-items-baseline flex-wrap align-content-start"
                         >
                             Border Countries:
-                            {state.borders.map((ele) => (
+                            {state.borders?.map((ele) => (
                                 <span
                                     className="btn m-1 lowdark"
                                     style={{
